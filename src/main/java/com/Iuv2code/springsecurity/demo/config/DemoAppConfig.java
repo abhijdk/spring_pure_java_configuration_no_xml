95100bce5a082637655f6c2ae294f8ecd393bb94
Abhinna <abhinnabehuria@outlook.com>
2024-09-17 15:11:22 +0530

    spring_pre_java_configuration_no_xml

---
+++ src/main/java/com/Iuv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -0,0 +1,25 @@
+package com.Iuv2code.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.Iuv2code.springsecurity.demo")
+public class DemoAppConfig {
+//    Define a bean for viewResolver
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: abhin
+  Date: 17-09-2024
+  Time: 00:05
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Spring_Security</title>
+</head>
+<body>
+    <h1>LUV2Code</h1>
+<p>next video is 12</p>
+</body>
+</html>
